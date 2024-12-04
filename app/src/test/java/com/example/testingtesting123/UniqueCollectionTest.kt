cee298237dff98c4960330d449d9c43f07663c31
Jack Martin <tus40499@temple.edu>
2024-12-04 15:09:24 -0500

    Completed TODOs

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,14 +16,17 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        collection.addItem(Item("item1"))
+        assert(collection.size() == 1) { "Item not added successfully" }
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
-
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("Item1")) // Should not be added due to case-insensitive equality
+        assert(collection.size() == 1) { "Duplicate item added" }
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
